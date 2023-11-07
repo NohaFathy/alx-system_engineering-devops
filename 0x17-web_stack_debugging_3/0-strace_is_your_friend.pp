e36745523a240202373eca6c47c6414a473411ce
Noha_Fathy <nohazaid@gmail.com>
2023-11-07 23:09:57 +0000

    finish

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes a WordPress site running on Apache2
+exec { 'fix-wordpress':
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php && sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+}
+
